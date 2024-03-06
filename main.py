d942e26a6de7883e9ad654f5ff2c7f05d312f0d6
Jons Hung <jonsphilogy@gmail.com>
2024-03-06 11:56:50 +0700

    added functionality for dynamically updated geometry

--- main.py
+++ main.py
@@ -6,7 +6,18 @@ import gc
 
 if __name__ == "__main__":
     dat = input.init_data("input.txt")
-    gg = graph.Graph(dat.dim, dat.geo_size, dat.geo_data)
-    algorithm.Astar(dat, gg)
+    gg = graph.Graph(dat.dim, dat.geo_data) 
+    #algorithm.Astar(dat, gg)
+
     gc.collect()
-    visualize.visualize_3d_graph(gg, dat.start, dat.end)
+    #visualize.visualize_3d_graph(gg, [dat.start, dat.end])
+
+    dat1 = input.init_data("input.txt")
+    gg1 = graph.Graph(dat1.dim, dat1.geo_data)
+
+    def dynamic_geo_updater(graph_list: list[graph.Graph], dat: list):
+        for g in graph_list:
+            g.partial_reset()
+            g.dynamic_geo(2, 1)
+            algorithm.Astar(dat1, gg1)
+    visualize.updatable([gg1], [dat1], dynamic_geo_updater, 1000)
--- visualize.py
+++ visualize.py
@@ -1,7 +1,12 @@
 import matplotlib.pyplot as plt
-import matplotlib.colors as colorplt
+from matplotlib.animation import FuncAnimation
 from graph import Graph
+from input import InputData
 import numpy as np
+from functools import partial
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
 
 def explode(data):
     size = np.array(data.shape)*2
@@ -14,19 +19,30 @@ def rgbtostring(rgba: list):
         rgba.append(255)
     return '#%02x%02x%02x%02x' % (rgba[0], rgba[1], rgba[2], rgba[3])
 
-def visualize_3d_graph(grid: Graph, start: list, end: list, show_expansion=False, node_scale=0.95):
-    dim = grid.dim.copy()
-    fig = plt.figure()
-    fig.tight_layout()
-    ax = fig.add_subplot(projection='3d')
-    fig.tight_layout()
+def plot_voxel(voxels, colors, labels, scale):
+    x, y, z = np.indices(np.array(voxels.shape) + 1).astype(float) // 2
+    q = 1 - scale
+    x[0::2, :, :] += q
+    y[:, 0::2, :] += q
+    z[:, :, 0::2] += q
+    x[1::2, :, :] += scale
+    y[:, 1::2, :] += scale
+    z[:, :, 1::2] += scale
     
+    ret = ax.voxels(x, y, z, voxels, facecolors=colors, edgecolors="k", shade=False)
+    ax.legend(labels=labels)
+    ax.set_aspect('equal', adjustable='box')
+    ax.axis('off')
+    return ret
+
+def build_voxel_map(grid: Graph, checkpoints: list[list[int]], show_expansion=False) -> list:
+    dim = grid.dim.copy()
+
     vox_grid = np.zeros((dim[0], dim[1], 2))
     colors = np.ndarray((dim[0], dim[1], 2), dtype=object)
-    rstr = [['white']] * grid.geo_size
-    for i in range(grid.geo_size):
-        r = list(np.random.choice(range(256), size=3))
-        #r.append(127)
+    rstr = [['white']] * len(grid.geo_data)
+    for i in range(len(grid.geo_data)):
+        r = list(np.random.choice(range(127), size=3))
         rstr[i] = rgbtostring(r)
     colors[:, :, :] = rgbtostring([255, 255, 255, 127]) # white
 
@@ -49,48 +65,56 @@ def visualize_3d_graph(grid: Graph, start: list, end: list, show_expansion=False
                 elif(grid.grid[idx].block == -1):  # expanded
                     colors[x, y, 0] = rgbtostring([255, 0, 0, 127])
 
-    found = False
-    if(end != None and len(end) >= 2):
-        traceback = end
+    if(checkpoints[-1] != None and len(checkpoints[-1]) >= 2):
+        traceback = checkpoints[-1]
         # in case path not found, still display start, stop
-        vox_grid[end[0], end[1], 1] = True
-        vox_grid[start[0], start[1], 1] = True
+        vox_grid[traceback[0], traceback[1], 1] = True
+        vox_grid[checkpoints[0][0], checkpoints[0][1], 1] = True
 
         while(grid.in_boundary(traceback)):
-            if(traceback[0] == start[0] and traceback[1] == start[1]):
-                found = True
-            
             vox_grid[traceback[0], traceback[1], 0] = False    # undo underlying node of path, saving compute resource
             vox_grid[traceback[0], traceback[1], 1] = True
             colors[traceback[0], traceback[1], 1] = rgbtostring([0, 0, 255, 255])
             n_node = grid.grid[grid.to_local_coord(traceback)]
             traceback = n_node.from_node
-        
         # recolor start and end
-        colors[end[0], end[1], 1] = rgbtostring([255, 0, 255])
-        colors[start[0], start[1], 1] = rgbtostring([255, 0, 255])
-    
-    filled = explode(vox_grid)
-    fcolors = explode(colors)
+        colors[checkpoints[-1][0], checkpoints[-1][1], 1] = rgbtostring([255, 0, 255])
+        colors[checkpoints[0][0], checkpoints[0][1], 1] = rgbtostring([255, 0, 255])
+    ex_voxels = explode(vox_grid)
+    ex_colors = explode(colors)
+    return [ex_voxels, ex_colors]
 
-    # offsetting nodes
-    x, y, z = np.indices(np.array(filled.shape) + 1).astype(float) // 2
-    q = 1 - node_scale
-    x[0::2, :, :] += q
-    y[:, 0::2, :] += q
-    z[:, :, 0::2] += q
-    x[1::2, :, :] += node_scale
-    y[:, 1::2, :] += node_scale
-    z[:, :, 1::2] += node_scale
+def visualize_3d_graph(grid: Graph, checkpoints: list[list[int]], show_expansion=False, node_scale=0.95):
+    plot_data = build_voxel_map(grid, checkpoints, show_expansion)
+    labels = [''] * 4
+    labels[0] = "size=[%d, %d]" % (grid.dim[0] - 1, grid.dim[1] - 1)
+    labels[1] = "start=%s" % (checkpoints[0])
+    labels[2] = "end=%s" % (checkpoints[-1])
+    labels[3] = "cost=%.2f" % (grid.grid[grid.to_local_coord(checkpoints[-1])].cost)
+    plot_voxel(plot_data[0], plot_data[1], labels, node_scale)
+    plt.show()
+
+def update(frame, graph_list: list[Graph], input_list: list[InputData], callback):
+
+    i = frame % len(graph_list)
+    grid = graph_list[i]
+    input_data = input_list[i]
     
-    ax.voxels(x, y, z, filled, facecolors=fcolors, edgecolors="k", shade=False)
-    labels = [''] * 5
-    labels[0] = "size=%s" % (grid.dim)
-    labels[1] = "start=%s" % (start)
-    labels[2] = "end=%s" % (end)
-    labels[3] = "found=%s" % ("true" if found else "false")
-    labels[4] = "cost=%d" % (grid.grid[grid.to_local_coord(end)].cost if found else -1)
-    ax.legend(labels=labels)
-    ax.set_aspect('equal', adjustable='box')
-    ax.axis('off')
+    checkpoints = [input_data.start]
+    for x in input_data.pickup:
+        checkpoints.append(x)
+    checkpoints.append(input_data.end)
+
+    voxel_map = build_voxel_map(graph_list[i], checkpoints)
+    labels = [''] * 4
+    labels[0] = "size=[%d, %d]" % (input_data.dim[0], input_data.dim[1])
+    labels[1] = "start=%s" % (checkpoints[0])
+    labels[2] = "end=%s" % (checkpoints[-1])
+    labels[3] = "cost=%.2f" % (grid.grid[grid.to_local_coord(checkpoints[-1])].cost)
+    ax.clear()
+    callback(graph_list, checkpoints)
+    return plot_voxel(voxel_map[0], voxel_map[1], labels, 0.95)
+
+def updatable(graph_list: list, input_list: list[InputData], callback, interval=1000):
+    ani = FuncAnimation(fig, partial(update, graph_list=graph_list, input_list=input_list, callback=callback), interval=interval, cache_frame_data=False)
     plt.show()
